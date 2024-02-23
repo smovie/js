21f36002df7842435396878ec22079dee70f11b2
smovie <z6@msn.cn>
2024-02-23 12:07:03 +0800

    Update RemoveWebLimits.user.js

--- user/RemoveWebLimits.user.js
+++ user/RemoveWebLimits.user.js
@@ -4,7 +4,7 @@
 // @name:zh-CN        网页限制解除（精简优化版）
 // @description       解除大部分网站禁止复制、剪切、选择文本、右键菜单的限制。
 // @author            Cat73  xinggsf
-// @version           1.5.7
+// @version           1.5.8
 // @license           LGPLv3
 // @include           https://www.zhihu.com/*
 // @include           https://www.bilibili.com/read/*
@@ -56,6 +56,7 @@
 // @include           *.wjys.cc/*
 // @include           *.dm233.cc/*
 // @include           *.gqyy8.com*
+// @include           *.dietitianbread.com/*
 // @grant             GM_addStyle
 // @run-at            document-start
 // ==/UserScript==
